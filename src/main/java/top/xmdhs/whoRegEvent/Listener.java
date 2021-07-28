a354586897c0eaf59f71ea539a13523756051a63
xmdhs <xmdhss@gmail.com>
2021-07-28 23:19:14 +0800

    更易查询

--- src/main/java/top/xmdhs/whoRegEvent/Listener.java
+++ src/main/java/top/xmdhs/whoRegEvent/Listener.java
@@ -2,30 +2,34 @@ package top.xmdhs.whoRegEvent;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
-import org.bukkit.event.Event;
-import org.bukkit.event.EventPriority;
+import org.bukkit.event.HandlerList;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.RegisteredListener;
 
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 
 public class Listener implements org.bukkit.event.Listener {
 
     public void regEvent(String eventName, CommandSender send) throws ClassNotFoundException {
-        Class<?> c = Class.forName(eventName);
-        AtomicBoolean isRun = new AtomicBoolean(false);
-        Bukkit.getPluginManager().registerEvent((Class<? extends Event>) c, this, EventPriority.MONITOR, (listener, event) -> {
-            if (isRun.get()){
-                event.getHandlers().unregister(Main.p);
-                return;
-            }
-            send.sendMessage("以下插件注册了 " + eventName);
-            for (RegisteredListener p :event.getHandlers().getRegisteredListeners()){
-                if (p.getPlugin().equals(Main.p)){
-                    continue;
+        Class<?> ec = Class.forName(eventName);
+
+        Plugin[] pl = Bukkit.getPluginManager().getPlugins();
+        for (Plugin p : pl) {
+            ArrayList<RegisteredListener> s = HandlerList.getRegisteredListeners(p);
+            Bukkit.getScheduler().runTaskAsynchronously(Main.p, () -> {
+                for (RegisteredListener r : s) {
+                    Method[] ml = r.getListener().getClass().getMethods();
+                    for (Method m : ml) {
+                        for (Class<?> c : m.getParameterTypes()) {
+                            if (ec.equals(c)) {
+                                send.sendMessage(p.getName()+ " " + r.getPriority().toString());
+                                return;
+                            }
+                        }
+                    }
                 }
-                send.sendMessage(p.getPlugin().getName());
-            }
-            isRun.set(true);
-        }, Main.p);
+            });
+        }
     }
 }
