30dcaef78f05a4eea9b34bc0f87ae096dfdc7c19
xmdhs <xmdhss@gmail.com>
2021-07-29 00:03:25 +0800

    更详细

--- src/main/java/top/xmdhs/whoRegEvent/Listener.java
+++ src/main/java/top/xmdhs/whoRegEvent/Listener.java
@@ -8,28 +8,29 @@ import org.bukkit.plugin.RegisteredListener;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Listener implements org.bukkit.event.Listener {
 
     public void regEvent(String eventName, CommandSender send) throws ClassNotFoundException {
         Class<?> ec = Class.forName(eventName);
 
-        Plugin[] pl = Bukkit.getPluginManager().getPlugins();
-        for (Plugin p : pl) {
-            ArrayList<RegisteredListener> s = HandlerList.getRegisteredListeners(p);
-            Bukkit.getScheduler().runTaskAsynchronously(Main.p, () -> {
-                for (RegisteredListener r : s) {
-                    Method[] ml = r.getListener().getClass().getMethods();
-                    for (Method m : ml) {
-                        for (Class<?> c : m.getParameterTypes()) {
-                            if (ec.equals(c)) {
-                                send.sendMessage(p.getName()+ " " + r.getPriority().toString());
-                                return;
-                            }
+        ArrayList<HandlerList> hl = HandlerList.getHandlerLists();
+        Set<String> sl = new HashSet<>();
+        for (HandlerList h : hl) {
+            for (RegisteredListener r : h.getRegisteredListeners()) {
+                for (Method m : r.getListener().getClass().getMethods()) {
+                    for (Class<?> c : m.getParameterTypes()) {
+                        if (ec.equals(c)) {
+                            sl.add(r.getPlugin().getName() + " " + r.getPriority().toString() + " " + r.getListener().getClass().getName());
                         }
                     }
                 }
-            });
+            }
+        }
+        for (String s : sl) {
+            send.sendMessage(s);
         }
     }
 }
