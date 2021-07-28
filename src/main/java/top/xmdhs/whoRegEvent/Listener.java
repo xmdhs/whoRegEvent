b58d02296e83942611979368153052a643e5ca88
xmdhs <xmdhss@gmail.com>
2021-07-28 17:04:24 +0800

    first commit

---
+++ src/main/java/top/xmdhs/whoRegEvent/Listener.java
@@ -0,0 +1,31 @@
+package top.xmdhs.whoRegEvent;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.Event;
+import org.bukkit.event.EventPriority;
+import org.bukkit.plugin.RegisteredListener;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class Listener implements org.bukkit.event.Listener {
+
+    public void regEvent(String eventName, CommandSender send) throws ClassNotFoundException {
+        Class<?> c = Class.forName(eventName);
+        AtomicBoolean isRun = new AtomicBoolean(false);
+        Bukkit.getPluginManager().registerEvent((Class<? extends Event>) c, this, EventPriority.MONITOR, (listener, event) -> {
+            if (isRun.get()){
+                event.getHandlers().unregister(Main.p);
+                return;
+            }
+            send.sendMessage("以下插件注册了 " + eventName);
+            for (RegisteredListener p :event.getHandlers().getRegisteredListeners()){
+                if (p.getPlugin().equals(Main.p)){
+                    return;
+                }
+                send.sendMessage(p.getPlugin().getName());
+            }
+            isRun.set(true);
+        }, Main.p);
+    }
+}
---
+++ src/main/java/top/xmdhs/whoRegEvent/Main.java
@@ -0,0 +1,46 @@
+package top.xmdhs.whoRegEvent;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements CommandExecutor {
+    protected static Plugin p;
+    private final Listener l = new Listener();
+
+    @Override
+    public void onEnable() {
+        p = this;
+        PluginCommand c = Bukkit.getPluginCommand("whoregevent");
+        assert c != null;
+        c.setExecutor(this);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if (commandSender instanceof Player) {
+            Player p = (Player) commandSender;
+            if (!p.isOp()) {
+                p.sendMessage("仅 op 可使用");
+                return false;
+            }
+        }
+        if (strings.length != 1) {
+            commandSender.sendMessage("/whoregevent <event class name>");
+            return false;
+        }
+        try {
+            l.regEvent(strings[0], commandSender);
+        } catch (ClassNotFoundException e) {
+            commandSender.sendMessage("没有这个事件");
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+}
