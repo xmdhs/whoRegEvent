0dbacc331bd794a56d5bf8853375568b4e2f8772
xmdhs <xmdhss@gmail.com>
2021-07-29 18:18:37 +0800

    fix

--- src/main/java/top/xmdhs/whoRegEvent/Main.java
+++ src/main/java/top/xmdhs/whoRegEvent/Main.java
@@ -40,15 +40,18 @@ public class Main extends JavaPlugin implements CommandExecutor {
         }
         Add a = map.get(strings[0]);
         if (a == null || !a.on) {
-            if (a == null){
-                try {
-                    a = new Add(strings[0], commandSender);
-                } catch (ClassNotFoundException e) {
-                    commandSender.sendMessage("没有这个事件");
-                    e.printStackTrace();
-                    return false;
-                }
+            if (a != null) {
+                a.on = true;
+                return true;
             }
+            try {
+                a = new Add(strings[0], commandSender);
+            } catch (ClassNotFoundException e) {
+                commandSender.sendMessage("没有这个事件");
+                e.printStackTrace();
+                return false;
+            }
+
             a.on = true;
             a.send = commandSender;
             map.put(strings[0], a);
@@ -61,8 +64,8 @@ public class Main extends JavaPlugin implements CommandExecutor {
                 e.printStackTrace();
             }
         } else {
-                a.on = false;
-                commandSender.sendMessage("已停止检测 " + strings[0]);
+            a.on = false;
+            commandSender.sendMessage("已停止检测 " + strings[0]);
         }
         return true;
     }
