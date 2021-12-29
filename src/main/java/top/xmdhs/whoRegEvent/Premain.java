07e5f475c48614aaf51f9f0b5eb11a2c6082e78c
xmdhs <xmdhss@gmail.com>
2021-12-29 20:41:57 +0800

    fix

--- src/main/java/top/xmdhs/whoRegEvent/Premain.java
+++ src/main/java/top/xmdhs/whoRegEvent/Premain.java
@@ -3,22 +3,27 @@ package top.xmdhs.whoRegEvent;
 import javassist.*;
 
 import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
 import java.lang.instrument.Instrumentation;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class Premain {
     public static void premain(String agentArgs, Instrumentation inst) {
+        HashSet<ClassLoader> h = new HashSet<>();
         ClassPool pp = ClassPool.getDefault();
         pp.importPackage("top.xmdhs.whoRegEvent.Callback");
         inst.addTransformer((loader, className, classBeingRedefined, protectionDomain, classfileBuffer) -> {
             if (className.startsWith("org/bukkit/event")) {
                 try {
                     ClassPool p = ClassPool.getDefault();
+                    if (!h.contains(loader)) {
+                        p.appendClassPath(new LoaderClassPath(loader));
+                        h.add(loader);
+                    }
                     CtClass c = p.makeClass(new ByteArrayInputStream(classfileBuffer));
                     if (c.isInterface()) {
                         return null;
                     }
-
                     CtClass cb = p.get("top.xmdhs.whoRegEvent.Callback");
                     CtField f = new CtField(cb, "callbackWre", c);
                     f.setModifiers(Modifier.PUBLIC);
